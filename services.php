39fa886f09189b4036b113dbaba6319d7b779455
Ricardo Iles <30296991+ricardoiles@users.noreply.github.com>
2022-09-15 23:03:09 -0500

    servicios al 90%

---
+++ services.php
@@ -0,0 +1,276 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>César Nuñez | Músico</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- <link rel="manifest" href="site.webmanifest"> -->
+    <link rel="shortcut icon" type="image" href="img/svg_icon/logocesarnuñez.svg">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <link rel="stylesheet" href="css/nice-select.css">
+    <link rel="stylesheet" href="css/audioplayer.css">
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/gijgo.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/slicknav.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/customstyles.css">
+    <!-- <link rel="stylesheet" href="css/responsive.css"> -->
+</head>
+
+<body>
+    <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+    <!-- header-start -->
+        <?php include 'components/navbar.php';?>
+    <!-- header-end -->
+
+    <!-- bradcam_area  -->
+    <div class="bradcam_area breadcam_bg_2">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="bradcam_text text-center">
+                        <h3>Mis servicios</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ bradcam_area  -->
+    <!-- music_area  -->
+    <div class="music_area music_gallery ">
+        <div class="container" style="margin-top:-100px">
+            <div class="container mb-3">
+                <!-- service section -->
+                <hr>
+                <p><h3>Servicios</h3></p>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card p-3 mb-2">
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row align-items-center">
+                                    <div class="ms-2 c-details">
+                                        <h4 class="mb-0"><b class="bogar-negrita">Músico de sesión</b></h4>
+                                    </div>
+                                </div>
+                                <div class="badge bg-primary"> <span>Servicio</span> </div>
+                            </div>
+                            <div class="bogar-card-body">
+                                <h5 class="">
+                                    <ul>
+                                        <li>- Acompañamiento de artistas o músicos para shows en vivo.</li>
+                                        <li>- Grabaciones de estudio.</li>
+                                        <li>- Creación de ideas y ritmos para tus canciones.</li>
+                                        <li>- Músico suplente - en vivo o ensayos.</li>
+                                    </ul>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card p-3 mb-2">
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row align-items-center">
+                                    <div class="ms-2 c-details">
+                                        <h4 class="mb-0"><b class="bogar-negrita">Trading</b></h4>
+                                    </div>
+                                </div>
+                                <div class="badge bg-primary"> <span>Servicio</span> </div>
+                            </div>
+                            <div class="bogar-card-body">
+                                <h5 class="">
+                                    <ul>
+                                        <li>- Uso de criptomonedas y manejo de plataformas de comercio / exchanges: (Bitso, Binance…etc).</li>
+                                        <li>- Grabaciones de estudio.</li>
+                                        <li>- Creación de ideas y ritmos para tus canciones.</li>
+                                        <li>- Músico suplente - en vivo o ensayos.</li>
+                                    </ul>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card p-3 mb-2">
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row align-items-center">
+                                    <div class="ms-2 c-details">
+                                        <h4 class="mb-0"><b class="bogar-negrita">Producción audiovisual</b></h4>
+                                    </div>
+                                </div>
+                                <div class="badge bg-primary"> <span>Servicio</span> </div>
+                            </div>
+                            <div class="bogar-card-body">
+                                <h5 class="">
+                                    <ul>
+                                        <li>- Edición de videos.</li>
+                                    </ul>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card p-3 mb-2">
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row align-items-center">
+                                    <div class="ms-2 c-details">
+                                        <h4 class="mb-0"><b class="bogar-negrita">Conferencias</b></h4>
+                                    </div>
+                                </div>
+                                <div class="badge bg-primary"> <span>Servicio</span> </div>
+                            </div>
+                            <div class="bogar-card-body">
+                                <h5 class="">
+                                    <ul>
+                                        <li>- Cómo recuperar tu salud con música y ley de la atracción / pensamientos positivos.</li>
+                                        <li>- Master Class Musicales - Clínicas.</li>
+                                    </ul>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end service section -->
+                <!-- clase section -->
+                <hr>
+                <p><h3>Clases</h3></p>
+                <div class="row">
+                    
+                    <div class="col-md-4">
+                        <div class="card p-3 mb-2">
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row align-items-center">
+                                    <div class="ms-2 c-details">
+                                        <h4 class="mb-0"><b class="bogar-negrita">Docencia / Educación</b></h4>
+                                    </div>
+                                </div>
+                                <div class="badge bg-success"> <span>Clase</span> </div>
+                            </div>
+                            <div class="bogar-card-body">
+                                <h5 class="">
+                                    <ul>
+                                        <li>- Estructuración de programas personalizados corta y larga duración.</li>
+                                        <li>- Clases A Domicilio.</li>
+                                        <li>- En mis instalaciones - <b class="bogar-negrita">Personalizadas 1-1.</b></li>
+                                        <li>- En mis instalaciones - <b class="bogar-negrita">Grupales.</b></li>
+                                        <li>- Clases Online: <b class="bogar-negrita">Materias:</b> 
+                                            Batería <br> Teoría musical: Rítmica y Métrica <br> Solfeo <br> Armonía <br> Entrenamiento Auditivo R-M-A: (rítmico.melódico.armónico).</li>
+                                    </ul>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card p-3 mb-2">
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row align-items-center">
+                                    <div class="ms-2 c-details">
+                                        <h4 class="mb-0"><b class="bogar-negrita">Contenido digital</b></h4>
+                                    </div>
+                                </div>
+                                <div class="badge bg-success"> <span>Clase</span> </div>
+                            </div>
+                            <div class="bogar-card-body">
+                                <h5 class="">
+                                    <ul>
+                                        <li>- Tutoriales/Clases gratuitas grabadas.</li>
+                                        <li>- Entrevistas.</li>
+                                        <li>- Cursos digitales <b>con costo</b>.</li>
+                                    </ul>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card p-3 mb-2">
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex flex-row align-items-center">
+                                    <div class="ms-2 c-details">
+                                        <h4 class="mb-0"><b class="bogar-negrita">Entrenamientos</b></h4>
+                                    </div>
+                                </div>
+                                <div class="badge bg-success"> <span>Clase</span> </div>
+                            </div>
+                            <div class="bogar-card-body">
+                                <h5 class="">
+                                    <ul>
+                                        <li>- Cómo entender la música, su escritura y los principales elementos para su interpretación.</li>
+                                        <li>- Desarrollo de sentido rítmico.</li>
+                                        <li>- Identificación de estructuras de canciones.</li>
+                                        <li>- Disfrutando de la música, sus ritmos.</li>
+                                    </ul>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end clase section -->
+            </div>
+        </div>
+    </div>
+    <!-- music_area end  -->
+
+    <!-- footer start -->
+        <?php include 'components/footer.php';?>
+    <!--/ footer end  -->
+
+    <!-- link that opens popup -->
+
+    <!-- JS here -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+            crossorigin="anonymous">
+        </script>
+        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="js/vendor/jquery-1.12.4.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/isotope.pkgd.min.js"></script>
+        <script src="js/ajax-form.js"></script>
+        <script src="js/waypoints.min.js"></script>
+        <script src="js/jquery.counterup.min.js"></script>
+        <script src="js/imagesloaded.pkgd.min.js"></script>
+        <script src="js/audioplayer.js"></script>
+        <script src="js/scrollIt.js"></script>
+        <script src="js/jquery.scrollUp.min.js"></script>
+        <script src="js/wow.min.js"></script>
+        <script src="js/nice-select.min.js"></script>
+        <script src="js/jquery.slicknav.min.js"></script>
+        <script src="js/jquery.magnific-popup.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/gijgo.min.js"></script>
+        <script src="js/slick.min.js"></script>
+        <!--contact js-->
+        <script src="js/contact.js"></script>
+        <script src="js/jquery.ajaxchimp.min.js"></script>
+        <script src="js/jquery.form.js"></script>
+        <script src="js/jquery.validate.min.js"></script>
+        <script src="js/mail-script.js"></script>
+
+        <script src="js/main.js"></script>
+    
+		<script>
+            $(function() {
+                $('audio').audioPlayer({
+
+                });
+            });
+        </script>
+</body>
+
+</html>
